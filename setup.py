0a3090d27b7f9d7b03c33bed891088897bc0cc96
Valmir França <vf79@yahoo.com>
2023-06-19 11:22:51 -0300

    turned to installable binary #9

--- setup.py
+++ setup.py
@@ -7,6 +7,11 @@ setup(
     description="Reward Point System for Dunder Mifflin",
     author="Valmir Franca",
     packages=find_packages(),
+    entry_points={
+        "console_scripts":[
+            "dundie = dundie.__main__:main"
+        ]
+    }
 )
 
 
