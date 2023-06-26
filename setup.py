ff5a7c37fd4bd3847d5857e555ed7b3b3abe7d43
Valmir França <vf79@yahoo.com>
2023-06-26 10:33:12 -0300

    Add CI GitHub Actions

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(
     long_description=read("README.md"),
     long_description_content_type = "text/markdown",
     author="Valmir Franca",
+    python_requires=">=3.9",
     packages=find_packages(),
     entry_points={
         "console_scripts":[
