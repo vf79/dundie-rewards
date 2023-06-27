162098d986f45c772f5056475098f033e784d8ab
Valmir França <vf79@yahoo.com>
2023-06-27 08:19:06 -0300

    Making an error

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+import pytest
+
 MARKER = """\
 unit: Mark unit tests
 integration: Mark integration tests
@@ -10,3 +12,10 @@ run: Select tests
 def pytest_configure(config):
     for line in MARKER.split("\n"):
         config.addinivalue_line('markers', line)
+
+
+@pytest.fixture(autouse=True)
+def go_to_tmpdir(request):
+    tmpdir = request.getfixturevalue("tmpdir")
+    with tmpdir.as_cwd():
+        yield #protocolo de generators
--- tests/test_01_load.py
+++ tests/test_01_load.py
@@ -4,7 +4,14 @@ from tests.constants import PEOPLE_FILE
 
 
 @pytest.mark.unit
-def test_load():
+@pytest.mark.high
+def test_load_positive_has_2_people():
     """Test load function"""
     assert len(load(PEOPLE_FILE))== 2
+
+
+@pytest.mark.unit
+@pytest.mark.high
+def test_load_positive_first_name_starts_with_j():
+    """Test load function"""
     assert load(PEOPLE_FILE)[0][0]=='J'
