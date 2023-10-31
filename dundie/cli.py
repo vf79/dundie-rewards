e3f1ef0e2129efb2091c17f7d04aa58e1d7312e1
Valmir Franca <vf79@yahoo.com>
2023-10-31 09:27:28 -0300

    Update cli

--- dundie/cli.py
+++ dundie/cli.py
@@ -89,6 +89,8 @@ def show(output, **query):
             table.add_column(key.title().replace("_", " "), style="green")
 
         for person in result:
+            person["value"] = f"{person['value']:.2f}"
+            person["balance"] = f"{person['balance']:.2f}"
             table.add_row(*[str(value) for value in person.values()])
 
     console = Console()
